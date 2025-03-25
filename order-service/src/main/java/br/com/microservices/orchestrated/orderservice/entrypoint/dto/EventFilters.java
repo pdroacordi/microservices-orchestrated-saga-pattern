4e89904097de98d8a80a30f8a42f629b2e9e6a07
Pedro Acordi <pedroacordi2@gmail.com>
2025-03-25 12:07:28 -0300

    feat: add builder pattern

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/entrypoint/dto/EventFilters.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/entrypoint/dto/EventFilters.java
@@ -1,12 +1,14 @@
 package br.com.microservices.orchestrated.orderservice.entrypoint.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class EventFilters {
 
     private String orderId;
